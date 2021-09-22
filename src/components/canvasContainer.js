63ee9486211cfb034a7f76ff9e612e2e6d5633e9
Leyght <savier811@gmail.com>
2021-09-22 20:05:48 +1200

    Raycaster set to centre when pointer lock is set

--- src/components/canvasContainer.js
+++ src/components/canvasContainer.js
@@ -1,18 +1,19 @@
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useThree, useFrame } from "@react-three/fiber";
 import * as THREE from "three";
-import {
-  PointerLockControls,
-  Box,
-} from "@react-three/drei";
-import React, { useRef, useState } from "react";
+import { PointerLockControls, Box, PerspectiveCamera } from "@react-three/drei";
+import React, { useRef, useState, useEffect } from "react";
 import { Physics, Debug } from "@react-three/cannon";
 import { Wall } from "./wall";
 import Sink from "./sink";
 import Tile from "./tile";
 import { Player } from "./player";
-import "../styles/canvasContainer.css"
+// import Reticle from "./Reticle";
+import "../styles/canvasContainer.css";
 
 const CanvasContainer = (props) => {
+  const controlsRef = useRef();
+  const isLocked = useRef(false);
+
   let tileLocation = new THREE.Vector3(46.155, 0.4, 46.155);
   const getTiles = () => {
     let content = [];
@@ -32,9 +33,28 @@ const CanvasContainer = (props) => {
     return content;
   };
 
+  const changeLocked = () => {
+    isLocked.current = !isLocked.current
+  }
+
+
   return (
-    <div id="canvas-container">
-      <Canvas camera={{ position: [0, 50, 100] }}>
+    <div onClick={changeLocked} id="canvas-container">
+      <Canvas
+        // camera={{ position: [0, 50, 100] }}
+        raycaster={{
+          computeOffsets: (_, { size: { width, height } }) => {
+            if (isLocked.current) {
+              return {
+                offsetX: width / 2,
+                offsetY: height / 2,
+              };
+            } else {
+              return null;
+            }
+          },
+        }}
+      >
         <ambientLight intensity={0.1} />
         <directionalLight intensity={6} color="white" position={[0, 100, 0]} />\
         {/* <OrbitControls /> */}
@@ -61,10 +81,56 @@ const CanvasContainer = (props) => {
           {/* </Debug> */}
           <Player />
         </Physics>
-        <PointerLockControls selector={"#canvas-container"} />
+        {/* <Camera /> */}
+        {/* <MouseReticle /> */}
+        <PointerLockControls
+          selector={"#canvas-container"}
+        />
       </Canvas>
     </div>
   );
 };
+
+function Camera() {
+  const mouseReticle = useRef();
+  const { camera, mouse } = useThree();
+  // initialize camera to look at origin.
+  useEffect(() => {
+    camera.lookAt(0, 0, 0);
+  }, [camera]);
+
+  return (
+    <PerspectiveCamera makeDefault position={[0, 50, 100]}>
+      {/* This reticle lives where the camera is pointing. 
+          This would be used in a first person environment */}
+      {/* <group position={[0, 0, -1]}>
+        <Reticle />
+      </group> */}
+    </PerspectiveCamera>
+  );
+}
+
+// Project a reticle of the mouse position onto the near plane.
+// A little janky because the rotation is off, so it gets cut.
+function MouseReticle() {
+  const { camera, mouse } = useThree();
+  const mouseReticle = useRef();
+
+  useFrame(() => {
+    if (mouseReticle.current) {
+      const vector = new THREE.Vector3(mouse.x, mouse.y, -0.8).unproject(
+        camera
+      );
+      mouseReticle.current.position.set(...vector.toArray());
+    }
+  });
+
+  return (
+    <mesh ref={mouseReticle}>
+      <sphereBufferGeometry args={[0.001, 100, 100]} />
+      <meshBasicMaterial color={"red"} />
+    </mesh>
+  );
+}
 
 export default CanvasContainer;
---
+++ src/components/reticle.js
@@ -0,0 +1,12 @@
+import React, { useRef, useState } from "react";
+import "../styles/reticle.css"
+
+const Reticle = (props) => {
+    return(
+        <div id="reticle">
+            <i className="far fa-dot-circle"></i>
+        </div>
+    )
+}
+
+export default Reticle
